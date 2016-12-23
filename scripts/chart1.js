0f311a9c0ab4f11c5fedfc3958a45b008f8824cd
Joel Kanerva <joel.kanerva@gmail.com>
2016-12-23 03:22:14 +0200

    Add files via upload

--- scripts/chart1.js
+++ scripts/chart1.js
@@ -1,11 +1,8 @@
 var choice = "scary";
-var chart1 = true;
 
 var noColor = "#BE3AF9"
 var yesColor = "#21F893"
-var maybeColor = "#15E7FC"
 
-var datasee;
 var globaldata;
 var circles;
 var forceStrength = 1;
@@ -25,6 +22,10 @@ $(window).scroll(function (event) {
     }
     else if (scroll > 4100 & scroll < 4500) {
     	secondChart();
+    } else if (scroll > 5100 & scroll < 5400) {
+    	thirdChart();
+    } else if (scroll > 6400 & scroll < 6550) {
+    	fourthChart();
     }
 });
 
@@ -34,7 +35,7 @@ $(window).scroll(function (event) {
 //
 var margin = {top: 50, right: 70, bottom: 50, left: 50},
     width = 950 - margin.left - margin.right,
-    height = 4500 - margin.top - margin.bottom;
+    height = 5500 - margin.top - margin.bottom;
 
 var svg = d3.select(".chart")
 	.append("svg")
@@ -57,7 +58,7 @@ var div = d3.select("body").append("div")
 var simulation = d3.forceSimulation();
 
 d3.queue()
-	.defer(d3.tsv, "data/ufotietokanta22122016.tsv")
+	.defer(d3.tsv, "data/ufotietokanta24122016.tsv")
 	.await(ready)
 
 
@@ -226,7 +227,21 @@ svg.append("foreignObject")
     .attr("height", 400)
     .attr("y", 2500)
     .append("xhtml:body")
-    .html("<h1>Havainnot kuukausitasolla</h1><p>Eniten havaintoja tehdään tammi-, elo- ja syyskuussa. Kuukaudet on numerojärjestyksessä vasemmasta ylänurkasta alkaen.</p><p style='color: #21F893;'>⚫ Kirkas päivänvalo</p><p style='color: #059451;'>⚫ Auringonlasku tai -nousu</p><p style='color: #BE3AF9;'>⚫ Hämärä</p><p style='color: #7505a8;'>⚫ Pimeä</p>");
+    .html("<h1>Tutki havaintoja kuukausitasolla</h1><p>Eniten havaintoja tehdään tammi-, elo- ja syyskuussa. Kuukaudet on numerojärjestyksessä vasemmasta ylänurkasta alkaen.</p><p style='color: #21F893;'>⚫ Kirkas päivänvalo</p><p style='color: #059451;'>⚫ Auringonlasku tai -nousu</p><p style='color: #BE3AF9;'>⚫ Hämärä</p><p style='color: #7505a8;'>⚫ Pimeä</p>");
+
+svg.append("foreignObject")
+    .attr("width", 900)
+    .attr("height", 400)
+    .attr("y", 3300)
+    .append("xhtml:body")
+    .html("<h1>Tutki havaintoja Suomessa ja naapurimaissa</h1><p>Eniten havaintoja on tehty Helsingissä. Myös muut suuret kaupungit korostuvat. Katso havainnot kartalta.</p><p style='color: #21F893;'>⚫ Kirkas päivänvalo</p><p style='color: #059451;'>⚫ Auringonlasku tai -nousu</p><p style='color: #BE3AF9;'>⚫ Hämärä</p><p style='color: #7505a8;'>⚫ Pimeä</p>");
+
+svg.append("foreignObject")
+    .attr("width", 900)
+    .attr("height", 300)
+    .attr("y", 4700)
+    .append("xhtml:body")
+    .html("<h1>Tutki havaintokuvia ja kertomuksia</h1><p>Vie osoitin havaintojen päälle tarkastellaksesi kuvia ja lisätietoja. Kaksoisklikkaamalla voit lukea koko havainnon kuvauksen.</p><p style='color: #21F893;'>⚫ Havaintokuvia</p><p style='color: #BE3AF9;'>⚫ Ei havaintokuvia</p>");
 
 
 	//Update colors based on filter selection
@@ -234,17 +249,6 @@ svg.append("foreignObject")
 		circles.attr("fill", fillColor)
 	}
 
-	svg.selectAll("texting")
-		.data(data)
-		.enter()
-		.append("text")
-		.text("Beautiful text")
-		.attr("dx", 10)
-		.attr("dy", ".35em")
-		.attr("x", 100)
-		.attr("y", 100)
-		.style("fill", "white")
-		.style("font-family", "helvetica")
 
 
 }
@@ -258,7 +262,7 @@ function secondChart(error, data) {
  	   .rangeRound([100, width-margin.right-230])
 
 	y = d3.scaleLinear()
-    	.rangeRound([3500, 2900])
+    	.rangeRound([3340, 2840])
 
 	y.domain([3, 0]);
 	x.domain([0, 3]);
@@ -313,5 +317,177 @@ function secondChart(error, data) {
  	}
 }
 
+function thirdChart(error, data) {
+
+	data = globaldata;
+
+	x = d3.scaleLinear()
+ 	   .rangeRound([100, width-margin.right-230])
+
+	y = d3.scaleLinear()
+    	.rangeRound([4500, 3640])
+
+	y.domain([59.7, 70]);
+	x.domain([19.5, 32]);
+
+	latForce = function(d) {
+		if (d.Lat < 59.7) {
+			return -100;
+		} else {
+			return x(d.Lat);
+		}
+	}
+
+	lonForce = function(d) {
+		if (d.Lon < 19.5 | d.Lon > 32) {
+			return -100;
+		} else {
+			return x(d.Lon);
+		}
+	}
+
+	simulation
+		.force("x", d3.forceX(function(d) { return x(d.Lon)}).strength(forceStrength / 10))
+		.force("y", d3.forceY(function(d) { return y(d.Lat)}).strength(forceStrength / 10))
+		.force("collide", d3.forceCollide(4));
 
 
+	var fillColor2 = function(d) {
+		if(d["Valoisuus"] == "kirkas päivänvalo") {
+			return yesColor;
+		} else if (d["Valoisuus"] == "auringonlasku_tai_nousu") {
+			return "#059451";	
+		} else if (d["Valoisuus"] == "hämärä") {
+				return "#BE3AF9";
+		} else {
+				return "#7505a8";
+			}
+	}
+
+	circles
+		.attr("fill", fillColor2)
+        .on("dblclick", dblclick);
+
+	function ticked() {
+		svg.selectAll(".havainnot")
+			.attr("cx", function(d) {
+				return d.x;
+			})
+			.attr("cy", function(d) {
+				return d.y;
+			})
+	}
+
+	simulation
+		.nodes(data)
+		.on("tick", ticked)
+		.alpha(0.65)
+		.restart()
+
+	circles.on("click", function(d) {
+		console.log(d);
+	})
+
+
+
+
+	function dblclick(a){
+    	window.open(a.URL, '_blank');
+ 	}
+}
+
+function fourthChart(error, data) {
+
+	data = globaldata;
+
+	x = d3.scaleLinear()
+ 	   .rangeRound([100, width-margin.right-230])
+
+	y = d3.scaleLinear()
+    	.rangeRound([5800, 5000])
+
+	y.domain([3, 0]);
+	x.domain([0, 3]);
+
+	forceXkuvat = function(d) {
+		if (d["havaintokuvat"] == "Kyllä") {
+			return width*0.23;
+		} else {
+			return width*0.65;
+		}
+	}
+
+	simulation
+		.force("x", d3.forceX(forceXkuvat).strength(forceStrength / 10))
+		.force("y", d3.forceY(function(d) { return 5100 }).strength(forceStrength / 10))
+		.force("collide", d3.forceCollide(4));
+
+
+	var fillColor2 = function(d) {
+		if(d["havaintokuvat"] == "Kyllä") {
+			return yesColor;
+		} else {
+			return noColor;
+	}}
+
+	circles
+		.attr("fill", fillColor2)
+        .on("dblclick", dblclick);
+
+	function ticked() {
+		svg.selectAll(".havainnot")
+			.attr("cx", function(d) {
+				return d.x;
+			})
+			.attr("cy", function(d) {
+				return d.y;
+			})
+	}
+
+	simulation
+		.nodes(data)
+		.on("tick", ticked)
+		.alpha(0.65)
+		.restart()
+
+	circles.on("click", function(d) {
+		console.log(d);
+
+		})
+
+	//If havaintokuvia
+
+	circles = svg.selectAll(".havainnot")
+		.on("mouseover", function(d) {		
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .99);
+
+            if (d["havaintokuvat"] == "Kyllä") {
+            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havaintokuva:</strong><br /><img src=" + d["kuva1"] +">" )	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
+            }
+
+            else {
+            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havainnon kuvaus:</strong><br />" + d["kuvaus"] )	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
+            }
+
+            })					
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        })
+
+
+
+
+
+
+	function dblclick(a){
+    	window.open(a.URL, '_blank');
+ 	}
+}
